aa72e96f055f576c6620b588149013188d393638
Pedro Avalos <pavalos6401@gmail.com>
2021-05-17 22:57:35 -0500

    start moving to a more OOP structure

--- settings/groups.py
+++ settings/groups.py
@@ -9,59 +9,83 @@ from libqtile.lazy import lazy
 
 from settings.keys import k, keys
 
-# Dictionary with window rules.
-# Places windows that match to the appropriate group.
-matches_rule = {
-    "web": [],
-    "dev": [Match(wm_class=["code"])],
-    "doc": [],
-    "chat": [
-        Match(wm_class=["discord"]),
-        Match(wm_class=["zoom"]),
-        Match(wm_class=["skype"]),
-    ],
-    "mus": [Match(wm_class=["spotify"])],
-    "vid": [Match(wm_class=["vlc"])],
-    "gam": [
-        Match(wm_class=["Steam"]),
-        Match(wm_class=["lutris"]),
-        Match(wm_class=["supertuxkart"]),
-        Match(wm_class=["xonotic-sdl"]),
-    ],
-    "sys": [],
-    "vm": [Match(wm_class=["virt-manager"]), Match(wm_class=["remote-viewer"])],
-}
+
+class GroupsMaker:
+    def __init__(self):
+        # Dictionary of group labels, with their respective settings
+        self.groups_settings = dict(
+            web=dict(layout="monadtall", matches=[]),
+            dev=dict(layout="monadtall", matches=[Match(wm_class=["code"])]),
+            doc=dict(layout="monadtall", matches=[]),
+            chat=dict(
+                layout="stack",
+                matches=[
+                    Match(wm_class=["discord"]),
+                    Match(wm_class=["zoom"]),
+                    Match(wm_class=["skype"]),
+                ],
+            ),
+            mus=dict(layout="monadtall", matches=[Match(wm_class=["spotify"])]),
+            vid=dict(layout="max", matches=[Match(wm_class=["vlc"])]),
+            gam=dict(
+                layout="tile",
+                matches=[
+                    Match(wm_class=["Steam"]),
+                    Match(wm_class=["lutris"]),
+                    Match(wm_class=["supertuxkart"]),
+                    Match(wm_class=["xonotic-sdl"]),
+                ],
+            ),
+            sys=dict(layout="monadtall", matches=[]),
+            vm=dict(
+                layout="treetab",
+                matches=[
+                    Match(wm_class=["virt-manager"]),
+                    Match(wm_class=["remote-viewer"]),
+                ],
+            ),
+        )
+        self.groups = []
+        self.keybindings = []
+
+    def make_groups(self):
+        num = 1
+        for k, v in self.groups_settings.items():
+            self.groups.append(
+                Group(f"{num}", layout=v["layout"], label=k, matches=v["matches"])
+            )
+            num += 1
+        return self.groups
+
+    def make_keybindings(self, k):
+        # Create the keybindings related to groups
+        for i in self.groups:
+            self.keybindings.extend(
+                [
+                    # super + name = switch to group
+                    Key(
+                        [k.SUPER],
+                        i.name,
+                        lazy.group[i.name].toscreen(),
+                        desc="Switch to group {}".format(i.name),
+                    ),
+                    # super + shift + name = move focused window to group
+                    Key(
+                        [k.SUPER, k.SHIFT],
+                        i.name,
+                        lazy.window.togroup(i.name),
+                        desc="Move focused window to group {}".format(i.name),
+                    ),
+                ]
+            )
+        return self.keybindings
+
+
+# Temporary, moving to a more OOP structure
+groups_maker = GroupsMaker()
 
 # Groups I find useful in my workflow
-groups = [
-    Group("1", layout="monadtall", label="web", matches=matches_rule["web"]),
-    Group("2", layout="monadtall", label="dev", matches=matches_rule["dev"]),
-    Group("3", layout="monadtall", label="doc", matches=matches_rule["doc"]),
-    Group("4", layout="stack", label="chat", matches=matches_rule["chat"]),
-    Group("5", layout="monadtall", label="mus", matches=matches_rule["mus"]),
-    Group("6", layout="max", label="vid", matches=matches_rule["vid"]),
-    Group("7", layout="tile", label="gam", matches=matches_rule["gam"]),
-    Group("8", layout="monadtall", label="sys", matches=matches_rule["sys"]),
-    Group("9", layout="treetab", label="vm", matches=matches_rule["vm"]),
-]
+groups = groups_maker.make_groups()
 
 # Add group-related bindings to the keybindings
-for i in groups:
-    keys.extend(
-        [
-            # super + name = switch to group
-            Key(
-                [k.SUPER],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
-            # super + shift + name = move focused window to group
-            Key(
-                [k.SUPER, k.SHIFT],
-                i.name,
-                lazy.window.togroup(i.name),
-                desc="Move focused window to group {}".format(i.name),
-            ),
-        ]
-    )
+keys.extend(groups_maker.make_keybindings(k))
--- settings/layouts.py
+++ settings/layouts.py
@@ -7,43 +7,63 @@
 from libqtile import layout
 from settings.theme import LayoutSchemes
 
+
+class LayoutsMaker:
+    def __init__(self):
+        self.layouts = []
+        self.floating_layout = None
+
+    def make_layouts(self):
+        # Layouts I commonly use, with unused ones commented out
+        self.layouts = [
+            layout.MonadTall(**LayoutSchemes.default),
+            layout.Max(**LayoutSchemes.default),
+            layout.Tile(**LayoutSchemes.default, expand=False),
+            layout.Stack(**LayoutSchemes.default, num_stacks=2),
+            layout.TreeTab(**LayoutSchemes.tree_tab),
+            layout.Floating(**LayoutSchemes.floating),
+            # layout.Zoomy(**LayoutSchemes.default),
+            # layout.Bsp(**LayoutSchemes.default),
+            # layout.Columns(**LayoutSchemes.default),
+            # layout.Matrix(**LayoutSchemes.default),
+            # layout.MonadWide(**LayoutSchemes.default),
+            # layout.RatioTile(**LayoutSchemes.default),
+            # layout.VerticalTile(**LayoutSchemes.default),
+        ]
+        return self.layouts
+
+    def make_floating_layout(self):
+        # Floating layout for windows that should default to floating
+        self.floating_layout = layout.Floating(
+            auto_float_typesR=[
+                # Run the utility `xprop` to see the wm class and name of an X client
+                {"wmclass": "confirm"},
+                {"wmclass": "dialog"},
+                {"wmclass": "download"},
+                {"wmclass": "error"},
+                {"wmclass": "file_progress"},
+                {"wmclass": "notification"},
+                {"wmclass": "notify"},
+                {"wmclass": "popup_menu"},
+                {"wmclass": "splash"},
+                {"wmclass": "toolbar"},
+                {"wmclass": "confirmreset"},  # gitk
+                {"wmclass": "makebranch"},  # gitk
+                {"wmclass": "maketag"},  # gitkm
+                {"wname": "branchdialog"},  # gitk
+                {"wname": "pinentry"},  # GPG key password entry
+                {"wmclass": "ssh-askpass"},  # ssh-askpass
+            ],
+            **LayoutSchemes.floating
+        )
+        return self.floating_layout
+
+
+# Temporary, moving to a more OOP approach
+layouts_maker = LayoutsMaker()
+
 # Layouts i commonly use, with unused ones commented out
-layouts = [
-    layout.MonadTall(**LayoutSchemes.default),
-    layout.Max(**LayoutSchemes.default),
-    layout.Tile(**LayoutSchemes.default, expand=False),
-    layout.Stack(**LayoutSchemes.default, num_stacks=2),
-    layout.TreeTab(**LayoutSchemes.tree_tab),
-    layout.Floating(**LayoutSchemes.floating),
-    # layout.Zoomy(**layout_defaults),
-    # layout.Bsp(**layout_defaults),
-    # layout.Columns(**layout_defaults),
-    # layout.Matrix(**layout_defaults),
-    # layout.MonadWide(**layout_defaults),
-    # layout.RatioTile(**layout_defaults),
-    # layout.VerticalTile(**layout_defaults),
-]
+layouts = layouts_maker.make_layouts()
 
 # Floating layout for windows that should default to floating
-floating_layout = layout.Floating(
-    auto_float_typesR=[
-        # Run the utility `xprop` to see the wm class and name of an X client
-        {"wmclass": "confirm"},
-        {"wmclass": "dialog"},
-        {"wmclass": "download"},
-        {"wmclass": "error"},
-        {"wmclass": "file_progress"},
-        {"wmclass": "notification"},
-        {"wmclass": "notify"},
-        {"wmclass": "popup_menu"},
-        {"wmclass": "splash"},
-        {"wmclass": "toolbar"},
-        {"wmclass": "confirmreset"},  # gitk
-        {"wmclass": "makebranch"},  # gitk
-        {"wmclass": "maketag"},  # gitkm
-        {"wname": "branchdialog"},  # gitk
-        {"wname": "pinentry"},  # GPG key password entry
-        {"wmclass": "ssh-askpass"},  # ssh-askpass
-    ],
-    **LayoutSchemes.floating
-)
+floating_layout = layouts_maker.make_floating_layout()
